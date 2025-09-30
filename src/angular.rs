00fc13a17d52a2cf73af70f2993778bd70cf787e
Jakub Konka <kubkon@jakubkonka.com>
2025-09-30 03:21:55 +0200

    Bump extension API, remove windows path workaround (#60)

--- src/angular.rs
+++ src/angular.rs
@@ -119,14 +119,7 @@ impl AngularExtension {
     }
 
     fn get_current_dir() -> Result<PathBuf> {
-        let mut current_dir =
-            env::current_dir().map_err(|e| format!("Failed to get current directory: {}", e));
-
-        if let Ok(path) = current_dir {
-            current_dir = Ok(zed_ext::sanitize_windows_path(path));
-        }
-
-        current_dir
+        env::current_dir().map_err(|e| format!("Failed to get current directory: {}", e))
     }
 
     fn get_ng_probe_locations(worktree: Option<&zed::Worktree>) -> Vec<String> {
@@ -184,7 +177,7 @@ impl zed::Extension for AngularExtension {
 
         let server_path = self.server_script_path(language_server_id)?;
         let current_dir = env::current_dir().unwrap_or(PathBuf::new());
-        let full_path_to_server = zed_ext::sanitize_windows_path(current_dir).join(&server_path);
+        let full_path_to_server = current_dir.join(&server_path);
 
         let mut args = vec![full_path_to_server.to_string_lossy().to_string()];
         args.push("--stdio".to_string());
@@ -246,25 +239,3 @@ impl zed::Extension for AngularExtension {
 }
 
 zed::register_extension!(AngularExtension);
-
-/// Extensions to the Zed extension API that have not yet stabilized.
-mod zed_ext {
-    /// Sanitizes the given path to remove the leading `/` on Windows.
-    ///
-    /// On macOS and Linux this is a no-op.
-    ///
-    /// This is a workaround for https://github.com/bytecodealliance/wasmtime/issues/10415.
-    pub fn sanitize_windows_path(path: std::path::PathBuf) -> std::path::PathBuf {
-        use zed_extension_api::{current_platform, Os};
-
-        let (os, _arch) = current_platform();
-        match os {
-            Os::Mac | Os::Linux => path,
-            Os::Windows => path
-                .to_string_lossy()
-                .to_string()
-                .trim_start_matches('/')
-                .into(),
-        }
-    }
-}
